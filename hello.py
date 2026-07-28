1e4a983988d4e314b5e19caef749864c1ee5e01b
jaylinnen <jaylinnen9@gmail.com>
2026-07-27 21:30:12 -0400

    Initial commit

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hello, World!")
+
+name = input("What is your name?")
+print("Hello, ", name + "!")
+
+age = input("How old are you?")
+print("You are", age, "years old.")
+
+feel = input("How are you feeling today?")
+print("You are feeling", feel, "today.")
