5fab1d6c1ddc7951f5aa70b0a3b7f7a166e75ea5
jaylinnen <jaylinnen9@gmail.com>
2026-07-27 22:20:54 -0400

    Add user input

--- hello.py
+++ hello.py
@@ -8,3 +8,6 @@ print("You are", age, "years old.")
 
 feel = input("How are you feeling today?")
 print("You are feeling", feel, "today.")
+
+color = input("What is your favorite color?")
+print("Your favorite color is", color + ".")
